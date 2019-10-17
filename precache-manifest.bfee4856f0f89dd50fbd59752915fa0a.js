d905be579a5632b3ec7dd09f4a36694589ba9eb3
Marcelo Jara <marcelo@trazolabs.com>
2019-10-16 22:57:46 -0300

    Updates

---
+++ precache-manifest.bfee4856f0f89dd50fbd59752915fa0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82970ffb40e5744dd10e26b98480b58",
+    "url": "/counters/index.html"
+  },
+  {
+    "revision": "f4d6b263c97f343c2395",
+    "url": "/counters/static/css/main.288ee21c.chunk.css"
+  },
+  {
+    "revision": "8edcd1e41db59ba3ed8d",
+    "url": "/counters/static/js/2.d9cd6169.chunk.js"
+  },
+  {
+    "revision": "f4d6b263c97f343c2395",
+    "url": "/counters/static/js/main.c7e9a1e2.chunk.js"
+  },
+  {
+    "revision": "40481615da44dfa3ecfd",
+    "url": "/counters/static/js/runtime-main.f8bd1c32.js"
+  }
+]);
